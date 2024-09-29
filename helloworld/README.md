cf820039ef5108d0cf6025d88016d3713813816e
Gandla Bhargavi <barau.avril@gmail.com>
2024-09-29 20:21:12 +0530

    moved files to helloworld and deleted code_pratice

---
+++ helloworld/README.md
@@ -0,0 +1,18 @@
+# Coding Basics - Hello World
+
+This repository contains a simple "Hello, World!" program in C++ to start my coding journey.
+
+## How to Run
+
+1. **Compile the program**:
+    ```bash
+    g++ hello_world.cpp -o hello_world
+    ```
+
+2. **Run the compiled executable**:
+    ```bash
+    ./hello_world
+    ```
+
+## Output
+
---
+++ helloworld/hello_world.cpp
@@ -0,0 +1,22 @@
+/**
+ * @file hello_world.cpp
+ * @author Gandla Bhargavi (barau.avril@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2024-09-29
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include <iostream>
+using namespace std;
+/**
+ * @brief  main function of the program to print the hello world .
+ * 
+ * @return int 
+ */
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
