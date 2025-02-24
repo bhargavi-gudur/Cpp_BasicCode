d2eaab8f5742f20d1132801615d7f01fcf2d1b42
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-24 14:09:03 +0530

    Add sample programs demonstrating destructor and string pattern matching

---
+++ pratice code/pattern_Matcher/pattern_matcher.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file pattern_matcher.cpp
+ * @author Gandla Bhargavi
+ * @brief
+ * @version 0.1
+ * @date 2024-11-05
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <iostream>
+#include <string>
+using namespace std;
+
+class StringChecker
+{
+private:
+    string str_;
+
+public:
+    StringChecker(const string &str);
+    bool isAnBn() const;
+    ~StringChecker() {}
+};
+/**
+ * @brief Construct a new String Checker:: String Checker object
+ *
+ * @param str
+ */
+StringChecker::StringChecker(const string &str) : str_(str) {}
+/**
+ * @brief
+ *
+ * @return true
+ * @return false
+ */
+bool StringChecker ::isAnBn() const
+{
+    int n = str_.length();
+    int i = 0;
+
+    while (i < n && str_[i] == 'a')
+    {
+        i++;
+    }
+    if (i * 2 != n)
+    {
+        return false;
+    }
+    while (i < n && str_[i] == 'b')
+    {
+        i++;
+    }
+    return i == n;
+}
+/**
+ * @brief  main function
+ *
+ * @return int
+ */
+int main()
+{
+    string str = "aabb";
+    StringChecker objchecker(str);
+    cout << (objchecker.isAnBn() ? "Yes" : "No") << endl;
+    return 0;
+}
