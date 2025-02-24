2651a53ed18aae994eeb4bf16503a8658a21a5f5
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-24 13:23:38 +0530

    Add twoSum solution implementation with test cases

---
+++ pratice code/array_twosum/array_twosum.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file array_twosum.cpp
+ * @author gandla bhargavi
+ * @brief
+ * @version 0.1
+ * @date 2024-12-07
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> numMap; // To store the value and its index
+
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            int complement = target - nums[i];
+
+            // Check if complement exists in the map
+            if (numMap.find(complement) != numMap.end())
+            {
+                return {numMap[complement], i}; // Return indices of the pair
+            }
+
+            // Store the current number and its index
+            numMap[nums[i]] = i;
+        }
+
+        // Since the problem guarantees exactly one solution, we should never reach here
+        return {};
+    }
+};
+
+int main()
+{
+    // Test the Solution class
+    Solution solution;
+
+    // Example 1
+    vector<int> nums1 = {2, 7, 11, 15};
+    int target1 = 9;
+    vector<int> result1 = solution.twoSum(nums1, target1);
+    cout << "Result 1: [" << result1[0] << ", " << result1[1] << "]" << endl;
+
+    // Example 2
+    vector<int> nums2 = {3, 2, 4};
+    int target2 = 6;
+    vector<int> result2 = solution.twoSum(nums2, target2);
+    cout << "Result 2: [" << result2[0] << ", " << result2[1] << "]" << endl;
+
+    // Example 3
+    vector<int> nums3 = {3, 3};
+    int target3 = 6;
+    vector<int> result3 = solution.twoSum(nums3, target3);
+    cout << "Result 3: [" << result3[0] << ", " << result3[1] << "]" << endl;
+
+    return 0;
+}
