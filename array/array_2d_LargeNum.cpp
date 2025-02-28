9bff56298baa8d2f87d494cabe6a2f4ea2eb499a
Gandla Bhargavi <barau.avril@gmail.com>
2025-02-28 15:12:19 +0530

    Add C++ program to create and display a 2D array, and find the largest element

---
+++ array/array_2d_LargeNum.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file array_2d_LargeNum.cpp
+ * @author Gandla Bhargavi
+ * @brief 2D array in c++  with n rows and m columns
+ * and display the elements in the array and find the largest element in the array.
+ * @version 0.1
+ * @date 2025-02-27
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+const int ROWS = 4;
+const int COLS = 2;
+
+int largestElement(int arr[ROWS][COLS], int rows, int cols)
+{
+    int max = arr[0][0];
+  
+    cout << "arrayof matrix:" << endl;
+    for (int i = 0; i < ROWS; ++i)
+    {
+        for (int j = 0; j < COLS; ++j)
+        {
+            if (arr[i][j] > max)
+            {
+                max = arr[i][j];
+            }
+            cout << "arr[" << i << "][" << j << "]: " << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+
+    return max;
+}
+
+int main()
+{
+    int arr[ROWS][COLS] = {3, 1, 4, 1, 5, 9, 2, 6};
+    int rows = sizeof(arr) / sizeof(arr[0]);
+    int cols = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Largest element: \n"
+         << largestElement(arr,rows,cols) << endl;
+    return 0;
+}
