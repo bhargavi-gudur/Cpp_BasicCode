9235bad4e1d0bbe7bc566231f5d0f2a4a2224fdc
Gandla Bhargavi <barau.avril@gmail.com>
2025-05-25 13:23:35 +0530

    Fix typos in comments for clarity in linked list implementation

--- singlelinkedlist/singleLinkList_begin_end.cpp
+++ singlelinkedlist/singleLinkList_begin_end.cpp
@@ -1,9 +1,13 @@
 /**
  * @file singleLinkList_begin_end.cpp
  * @author gandla bhargavi
+ * 
  * @brief  link list adding node at begin  and end of the node
- * then transver the link list data.
+ * then treverse the link list data.
+ * 
  * @version 0.1
  * @date 2025-03-18
  *
- */  
+ */
+
+ 
