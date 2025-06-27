027763e5e1b33dd302bcb5a8f8be73020780f9d1
Gandla Bhargavi <barau.avril@gmail.com>
2025-06-27 15:10:52 +0530

    Add C++ program to demonstrate bitwise operations using extern "C" and classes

---
+++ OPPS_concepts/Bitwise_oops/bitwise.cpp
@@ -0,0 +1,63 @@
+/**
+ * @file bitwise.cpp
+ * @author Gandla Bhargavi
+ * @brief  Bitwise operations using extern "C" and class and object.
+ * @version 0.2
+ * @date 2025-02-28
+ *
+ * @copyright Copyright (c) 2025
+ *
+ */
+#include <iostream>
+using namespace std;
+extern "C"
+{
+    /**
+     * @brief  Bitwise operations using extern "C".
+     *
+     * @param num
+     */
+    void bitwise_operation(int *num)
+    {
+        int and_result = (*num) & 6; // 1100 & 0110 = 0100 (4)
+        cout << "Bitwise AND with 6 : " << and_result << endl;
+        int shift_left_result = (*num) << 2; // 1100 << 2 = 110000 (48)
+        cout << "Bitwise Shift Left by 2: " << shift_left_result << endl;
+        int shift_right_result = (*num) >> 2; // 1100  >> 2 = 0011 (3)
+        cout << "Bitwise Shift Right by 2: " << shift_right_result << endl;
+    }
+}
+/**
+ * @brief Bitwise class  and object to perform bitwise operations.
+ *
+ */
+class bitwise
+{
+public:
+    int *num;
+    void bitwise_operation(int *num)
+    {
+        int and_result = (*num) & 6; // 1100 & 0110 = 0100 (4)
+        cout << "Bitwise AND with 6 : " << and_result << endl;
+        int shift_left_result = (*num) << 2; // 1100 << 2 = 110000 (48)
+        cout << "Bitwise Shift Left by 2: " << shift_left_result << endl;
+        int shift_right_result = (*num) >> 2; // 1100  >> 2 = 0011 (3)
+        cout << "Bitwise Shift Right by 2: " << shift_right_result << endl;
+    }
+    ~bitwise() {}
+};
+/**
+ * @brief  Main function to demonstrate bitwise operations.
+ *
+ * @return int
+ */
+int main()
+{
+    int value = 12;
+    cout << "----------------extern c style------------" << endl;
+    bitwise_operation(&value);
+    cout << "--------------- class and obj ------------" << endl;
+    bitwise objbitwise;
+    objbitwise.bitwise_operation(&value);
+    cout << "------------------------------------------" << endl;
+}
