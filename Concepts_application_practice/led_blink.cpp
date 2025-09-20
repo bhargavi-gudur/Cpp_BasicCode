f1b50f25f4254b0a3cb0f15f498f0d0f826f3e6a
Gandla Bhargavi <barau.avril@gmail.com>
2025-09-20 09:28:33 +0530

    Refactor led_blink.cpp to improve code structure by removing unnecessary namespace usage and encapsulating LED blinking logic in a dedicated function.

--- Concepts_application_practice/led_blink.cpp
+++ Concepts_application_practice/led_blink.cpp
@@ -9,14 +9,36 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
-using namespace std;
+
+// For convenience
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::chrono::milliseconds;
+
+
+/**
+ * @brief  Function to simulate LED blinking.
+ * 
+ */
+void blink_led() {
+for (int i = 0; i < 5; i++) {
+  cout << "LED ON\n";
+  std::this_thread::sleep_for(std::chrono::milliseconds(500)); // delay
+  cout << "LED OFF\n";
+  std::this_thread::sleep_for(std::chrono::milliseconds(500));
+}
+
+
+/**
+ * @brief  Main function to execute the LED blink simulation.
+ * 
+ * 
+ * @return int 
+ */
 
 int main() {
-    for (int i = 0; i < 5; i++) {
-        cout << "LED ON\n";
-        this_thread::sleep_for(chrono::milliseconds(500)); // delay
-        cout << "LED OFF\n";
-        this_thread::sleep_for(chrono::milliseconds(500));
-    }
+  blink_led();
     return 0;
 }
