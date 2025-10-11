370347c431aab43fbaf6305cce9ddafd1270c1ba
Gandla Bhargavi <barau.avril@gmail.com>
2025-10-12 04:56:26 +0530

    Enhance documentation for areaofcircle class and its methods

--- classmodifier/classModifier.cpp
+++ classmodifier/classModifier.cpp
@@ -14,23 +14,26 @@
 using namespace std;
 
 /**
- * @brief  area of circle class 
- * 
+ * @brief  area of circle class to access the radius and area of circle function
+ * @class areaofcircle
+ * @details  class is a user-defined data type that represents a blueprint for
+ * creating objects.
+ * It encapsulates data members (attributes) and member functions
+ * (methods) that operate on the data.
  */
 class areaofcircle{
     public:
     double radius;
      areaofcircle(double rad) : radius(rad) {}
     double areaofcircle_fun()
-    {
-        
+    {     
         return 3.14* radius*radius;
-
     }
     ~areaofcircle(){}
 };
 /**
  * @brief main function created objected and access area of circle class
+ *
  * 
  * @return int 
  */
