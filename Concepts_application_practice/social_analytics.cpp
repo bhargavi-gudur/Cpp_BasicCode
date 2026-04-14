fcdca5968a95192355b05481c1171703c2f9fe92
Gandla Bhargavi <40709879+bhargavi-gudur@users.noreply.github.com>
2026-04-14 12:26:12 +0530

    Create social_analytics.cpp

---
+++ Concepts_application_practice/social_analytics.cpp
@@ -0,0 +1,112 @@
+/**
+ * @file social_analytics.cpp
+ * @author Gandla Bhargavi
+ * @brief
+ *   This program simulates social media analytics.
+ *   It counts likes, shares, and comments with current date.
+ * @date 14-04-2026
+ */
+
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+/**
+ * @brief Class for Analytics
+ */
+class Analytics {
+private:
+    int likes = 0;
+    int shares = 0;
+    int comments = 0;
+
+public:
+
+    // Add like
+    void addLike() {
+        likes++;
+        cout << "Liked 👍\n";
+    }
+
+    // Add share
+    void addShare() {
+        shares++;
+        cout << "Shared 🔁\n";
+    }
+
+    // Add comment
+    void addComment() {
+        comments++;
+        cout << "Comment added 💬\n";
+    }
+
+    // Show analytics
+    void showAnalytics() {
+
+        cout << "\n===== Analytics Report =====\n";
+
+        cout << "Likes: " << likes << endl;
+        cout << "Shares: " << shares << endl;
+        cout << "Comments: " << comments << endl;
+
+        // Show current date
+        time_t now = time(0);
+        tm *ltm = localtime(&now);
+
+        cout << "Date: "
+             << ltm->tm_mday << "-"
+             << 1 + ltm->tm_mon << "-"
+             << 1900 + ltm->tm_year << endl;
+    }
+};
+
+/**
+ * @brief Main function
+ */
+int main() {
+
+    Analytics a;
+    int choice;
+
+    cout << "===== Social Media Analytics =====\n";
+
+    do {
+        cout << "\n1. Like\n";
+        cout << "2. Share\n";
+        cout << "3. Comment\n";
+        cout << "4. View Analytics\n";
+        cout << "5. Exit\n";
+
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice) {
+
+            case 1:
+                a.addLike();
+                break;
+
+            case 2:
+                a.addShare();
+                break;
+
+            case 3:
+                a.addComment();
+                break;
+
+            case 4:
+                a.showAnalytics();
+                break;
+
+            case 5:
+                cout << "Thank you!\n";
+                break;
+
+            default:
+                cout << "Invalid choice!\n";
+        }
+
+    } while(choice != 5);
+
+    return 0;
+}
